ccc6a580981f3184efd76d78b51dbcf05078a957
joostremmers <mail@joostremmers.nl>
2021-06-29 21:50:35 +0200

    Update example.py

--- 6. Copulas (beta)/example.py
+++ 6. Copulas (beta)/example.py
@@ -42,7 +42,7 @@ validate = requests.get(url+"validate",headers={"Authorization":str(login.json()
 print(validate.json()["message"]) # Can always be requested to check the API response
 
 # ----------------------------------------------------------------------------
-# 4. DISTRIBUTION FITTING
+# 4. COPULA FITTING
 # ----------------------------------------------------------------------------
 
 # 4.1 Data import
@@ -52,6 +52,6 @@ copula_data = json.load(open('./input.json','r'))
 analysis = requests.post(url+"copulas",json=copula_data,headers={"Authorization":str(login.json()['access_token'])})
 
 # 4.3 Convert strings to bytes value and save
-create_image("input_report",analysis.json()["report_1_png"],analysis.json()["report_2_pdf"])
+create_image("input_report",analysis.json()["report_1_png"],analysis.json()["report_1_pdf"])
 create_image("output_report",analysis.json()["report_2_png"],analysis.json()["report_2_pdf"])
 create_logfile("logfile",analysis.json()["log_string"])
